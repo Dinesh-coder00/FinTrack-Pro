10bc58a681316617a92bff92a9a4319ccf1ee9d5
Dinesh <dinesh@gmail.com>
2026-08-06 15:04:36 +0530

    Allow OPTIONS requests and update CORS settings

--- FintrackPro-Backend/src/main/java/com/fintrack/config/SecurityConfig.java
+++ FintrackPro-Backend/src/main/java/com/fintrack/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
             .sessionManagement(sm ->
                 sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
             		.requestMatchers(HttpMethod.POST,
             		        "/auth/register",
             		        "/auth/login",
